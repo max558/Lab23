24b6ab70586c8523ecd36891df290e05b07fe0fd
mivvlk <mivvlk@yandex.ru>
2021-12-03 13:27:58 +0300

    Добавьте файлы проекта.

---
+++ InPutData.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InPutDataSpace
+{
+    static public class InPutData
+    {
+
+        /* 
+         * Ввод строки. 
+         * str - Строковый параметр запроса
+         */
+        static public string EnterDataStr(string str)
+        {
+            string res = "";
+            Console.Write(str);
+            res = Console.ReadLine();
+            return res;
+        }
+
+        /* 
+         * Ввод целого числа. 
+         * str - Строковый параметр запроса, 
+         * positive - true только числа больше 0 
+         */
+        static public int EnterDataInt(string str, bool positive)
+        {
+            int res = 0;
+            Console.Write(str);
+            try
+            {
+                res = Convert.ToInt32(Console.ReadLine());
+                if ((positive) && (res < 0))
+                {
+                    res = EnterDataInt("Число меньше 0 попробуйте ввсти еще раз: ", positive);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка! {0}", ex.Message);
+                res = EnterDataInt("Попробуйте ввсти еще раз: ",positive);
+            }
+            return res;
+        }
+
+        /* 
+         * Ввод числа с дробной частью. 
+         * str - Строковый параметр запроса, 
+         * positive - true только числа больше 0 
+         */
+        static public double EnterDataDouble(string str, bool positive)
+        {
+            double res = 0;
+            Console.Write(str);
+            try
+            {
+                res = Convert.ToDouble(Console.ReadLine());
+                if ((positive) && (res < 0))
+                {
+                    res = EnterDataDouble("Число меньше 0 попробуйте ввсти еще раз: ", positive);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка! {0}", ex.Message);
+                res = EnterDataDouble("Попробуйте ввсти еще раз: ", positive);
+            }
+            return res;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InPutDataSpace;
+
+
+namespace Lab_23
+{
+    /*
+     * Разработать асинхронный метод для вычисления факториала числа. 
+     * В методе Main выполнить проверку работы метода.
+     */
+    class Program
+    {
+        static double n;
+        static void Factjrial(double x)
+        {
+            double res = 1;
+
+            for (int i = 1; i <= x; i++)
+            {
+                res = res * i;
+            }
+            Console.WriteLine("Факториал числа: {0} != {1}", x, res);
+        }
+        static void Main(string[] args)
+        {
+            /*
+             * "В методе Main выполнить проверку работы метода" - Не совсем понял о каком методе идет речь.
+             * Если проверка вводимых чисел (для факториала), то это сделано в отдельном классе и одельном файле при вводе числа. Как следствие в методе Main.
+             * Если проверять результат выполнения (переполнения памяти), то асинхронность теряет смысл, да и результат будет бесконечность (проверял на больших числах).
+             * Поэтому try-catch закоментировал
+             * 
+             * Для понятия работы асинхронности ввел вывод об окончании работ методов.
+             */
+
+            //Реализовал связь разных файлов, ссылками (чисто ради эксперимента, и дальнейшего развития наработок и их использования)
+            n = InPutData.EnterDataInt("Введите число факториала: ", true);
+            //try
+            //{
+                FactjrialAsync();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Ошибка! {0}", ex);
+            //}
+
+            Console.WriteLine("Main закончил работу!");
+            Console.ReadKey();
+        }
+
+        static async Task FactjrialAsync()
+        {
+            await Task.Run(() => Factjrial(n));
+            Console.WriteLine("FactjrialAsync закончил работу!");
+        }
+    }
+}
